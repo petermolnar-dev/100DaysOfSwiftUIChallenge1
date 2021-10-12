a7857f34958b1fc67c571b97357673ae72141329
Peter Molnar <petermolnar.hu@gmail.com>
2021-10-12 11:48:55 +0200

    UI is ready, logic needs to be implemented.

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var originalValue = ""
+    @State private var selectedInputMeasurement = 0
+    @State private var selectedOutputMeasurement = 0
+    
+    let temperatureMeasurements = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var convertedValue: Double {
+        // TODO: The conversion logic here.
+        0.0
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter the original value", text: $originalValue)
+                    Picker("Input measurement", selection: $selectedInputMeasurement) {
+                        ForEach(0..<temperatureMeasurements.count) {
+                            Text(self.temperatureMeasurements[$0])
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Converted value")) {
+                    Text("\(convertedValue, specifier: "%.2f")")
+                    Picker("Input measurement", selection: $selectedOutputMeasurement) {
+                        ForEach(0..<temperatureMeasurements.count) {
+                            Text(self.temperatureMeasurements[$0])
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationBarTitle("Unit Converter")
+        }
     }
 }
 
