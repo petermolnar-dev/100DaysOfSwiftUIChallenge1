72cd7fcbab5ec6ad12eeee59650c45c580f59e00
Peter Molnar <petermolnar.hu@gmail.com>
2021-10-12 11:30:42 +0200

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Peter Molnar on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
