c1194c7947d6b659390c97e7963daa0c34519415
Peter Molnar <petermolnar.hu@gmail.com>
2021-10-13 11:59:17 +0200

    Adding the different conversions and the business logic to the app.

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -14,9 +14,58 @@ struct ContentView: View {
     
     let temperatureMeasurements = ["Celsius", "Fahrenheit", "Kelvin"]
     
-    var convertedValue: Double {
-        // TODO: The conversion logic here.
-        0.0
+    var originalValueNumber: Double {
+        Double(originalValue) ?? 0
+    }
+    
+    func unit(from selectedMeasurement: Int) -> UnitTemperature {
+        switch selectedMeasurement {
+        case 1:
+            return UnitTemperature.fahrenheit
+        case 2:
+            return UnitTemperature.kelvin
+        default:
+            return UnitTemperature.celsius
+        }
+    }
+    
+    func convertToKelvin(value: Double, fromUnit: UnitTemperature) -> Double {
+        if fromUnit == UnitTemperature.celsius {
+            return value + 273.15
+        }
+        if fromUnit == UnitTemperature.fahrenheit {
+            return (value * 0.55555555555556) + 255.37222222222427
+        }
+        return value
+    }
+    
+    func convertFromKelvin(value: Double, toUnit: UnitTemperature) -> Double {
+        if toUnit == UnitTemperature.celsius {
+            return value - 273.15
+        }
+        if toUnit == UnitTemperature.fahrenheit {
+            return (value - 255.37222222222427) / 0.55555555555556
+        }
+        return value
+    }
+    
+    func convert(value: Double, fromUnit: UnitTemperature, toUnit: UnitTemperature) -> Double {
+        if fromUnit == toUnit {
+            return value
+        }
+        let kelvinValue = convertToKelvin(value: value, fromUnit: fromUnit)
+        let convertedValue = convertFromKelvin(value: kelvinValue, toUnit: toUnit)
+        return convertedValue
+    }
+    
+    var convertedValue: String {
+        let originalUnit = unit(from: selectedInputMeasurement)
+        let convertedUnit = unit(from: selectedOutputMeasurement)
+        let convertedValue = convert(value: originalValueNumber,
+                                     fromUnit: originalUnit,
+                                     toUnit: convertedUnit)
+        let originalMeasure = Measurement(value: convertedValue, unit: convertedUnit)
+        return originalMeasure.description
     }
     
     var body: some View {
@@ -32,7 +81,7 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 Section(header: Text("Converted value")) {
-                    Text("\(convertedValue, specifier: "%.2f")")
+                    Text("\(convertedValue)")
                     Picker("Input measurement", selection: $selectedOutputMeasurement) {
                         ForEach(0..<temperatureMeasurements.count) {
                             Text(self.temperatureMeasurements[$0])
